63b559efc3bf6cf2943457a808ffb41423061e19
Чан Тоан <c.toan@corp.mail.ru>
2020-05-01 14:50:50 +0300

    Исправил название переменных

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int customer_account=100;
-        int sum_of_refill=2000;
-        int refill=customer_account+sum_of_refill;
-        int personal_bonus;
-        if(sum_of_refill>=1000)
-        {
-            personal_bonus=refill/100;
-        }
-        else
-        {
-            personal_bonus=0;
+        int customerAccount = 100;
+        int sumOfRefill = 2000;
+        int refill = customerAccount + sumOfRefill;
+        int personalBonus;
+        if (sumOfRefill >= 1000) {
+            personalBonus = refill / 100;
+        } else {
+            personalBonus = 0;
 
         }
-        System.out.println(personal_bonus);
+        System.out.println(personalBonus);
     }
 }
